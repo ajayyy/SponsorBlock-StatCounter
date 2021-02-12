cba9fd6cd492be7b99dca7266b665fb8734a3762
Ajay Ramachandran <dev@ajay.app>
2021-02-11 19:43:19 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const URL = "https://sponsor.ajay.app/api/getTotalStats";
+const target = 1000000;
+
+let firstSubmissionCount = null;
+let firstCountTime = null;
+
+async function main() {
+    const submissionsElem = document.getElementById("submissions");
+    const rateElem = document.getElementById("rate");
+    const etaElem = document.getElementById("eta");
+
+    while (true) {
+        const result = await (await fetch(URL)).json();
+
+        if (result.totalSubmissions){
+            submissionsElem.innerText = result.totalSubmissions;
+
+            if (firstSubmissionCount === null) {
+                firstSubmissionCount = result.totalSubmissions;
+                firstCountTime = Date.now();
+            } else if (firstSubmissionCount !== result.totalSubmissions) {
+                // Per minute
+                const submissionDifference = result.totalSubmissions - firstSubmissionCount;
+                const timePassed = Date.now() - firstCountTime;
+                const rpm = submissionDifference / (timePassed / 60000);
+                rateElem.innerText = rpm;
+
+                // In hours
+                const eta =  (target - result.totalSubmissions) / (rpm * 60);
+                etaElem.innerText = eta;
+            }
+        }
+
+        await new Promise((resolve) => setTimeout(resolve, 500));
+    }
+}
+
+main();
