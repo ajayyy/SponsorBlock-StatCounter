eb4bdcbe9b4519c9286d7bfc83920ca4b0f3877f
Ajay Ramachandran <dev@ajay.app>
2021-02-20 17:11:41 -0500

    Switch to using non async function

--- script.js
+++ script.js
@@ -4,37 +4,35 @@ const target = 1000000;
 let firstSubmissionCount = null;
 let firstCountTime = null;
 
-async function main() {
-    const submissionsElem = document.getElementById("submissions");
-    const rateElem = document.getElementById("rate");
-    const etaElem = document.getElementById("eta");
+const submissionsElem = document.getElementById("submissions");
+const rateElem = document.getElementById("rate");
+const etaElem = document.getElementById("eta");
 
-    while (true) {
-        const result = await (await fetch(URL)).json();
+function updateData(result) {
+    if (result && result.totalSubmissions){
+        submissionsElem.innerText = result.totalSubmissions;
 
-        if (result.totalSubmissions){
-            submissionsElem.innerText = result.totalSubmissions;
+        if (firstSubmissionCount === null) {
+            firstSubmissionCount = result.totalSubmissions;
+            firstCountTime = Date.now();
+        } else if (firstSubmissionCount !== result.totalSubmissions) {
+            // Per minute
+            const submissionDifference = result.totalSubmissions - firstSubmissionCount;
+            const timePassed = Date.now() - firstCountTime;
+            const rpm = submissionDifference / (timePassed / 60000);
+            rateElem.innerText = rpm;
 
-            if (firstSubmissionCount === null) {
-                firstSubmissionCount = result.totalSubmissions;
-                firstCountTime = Date.now();
-            } else if (firstSubmissionCount !== result.totalSubmissions) {
-                // Per minute
-                const submissionDifference = result.totalSubmissions - firstSubmissionCount;
-                const timePassed = Date.now() - firstCountTime;
-                const rpm = submissionDifference / (timePassed / 60000);
-                rateElem.innerText = rpm;
+            // In hours
+            const eta =  (target - result.totalSubmissions) / (rpm * 60);
+            etaElem.innerText = eta;
 
-                // In hours
-                const eta =  (target - result.totalSubmissions) / (rpm * 60);
-                etaElem.innerText = eta;
-
-                if (result.totalSubmission >= 1000000 && !confetti.isRunning()) confetti.start(); 
-            }
+            if (result.totalSubmission >= 1000000 && !confetti.isRunning()) confetti.start(); 
         }
-
-        await new Promise((resolve) => setTimeout(resolve, 500));
     }
+
+    setTimeout(async () => {
+        (await fetch(URL)).json().then(updateData);
+    }, 500);
 }
 
-main();
+updateData();
